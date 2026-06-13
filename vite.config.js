7dc0520cccedb6fc44bc853cafba3b4e33a22845
Maxwell Howe <mhowe.gis@gmail.com>
2026-06-13 00:53:42 -0400

    chore: configure adapter-static with BASE_PATH switch + vitest config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [
@@ -10,11 +10,23 @@ export default defineConfig({
 				runes: ({ filename }) =>
 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
-
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
+			adapter: adapter({
+				pages: 'build',
+				assets: 'build',
+				fallback: 'index.html',
+				precompress: false,
+				strict: true
+			}),
+			paths: {
+				base: process.env.BASE_PATH ?? ''
+			},
+			prerender: {
+				handleHttpError: 'warn'
+			}
 		})
-	]
+	],
+	test: {
+		include: ['tests/**/*.{test,spec}.{js,ts}'],
+		environment: 'jsdom'
+	}
 });
